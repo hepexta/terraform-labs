686dedbc22783f398a844de82a5012a24cacd67f
Sergei <hepexta@gmail.com>
2023-08-13 16:08:55 +0200

    Add new network interface

--- network-interface.tf
+++ network-interface.tf
@@ -14,6 +14,6 @@ resource "azurerm_network_interface" "mynetworkinterface" {
 }
 
 resource "azurerm_network_interface_security_group_association" "mynisgassociation" {
-  network_interface_id      = azurerm_network_interface.myNetworkInterface.id
+  network_interface_id      = azurerm_network_interface.mynetworkinterface.id
   network_security_group_id = azurerm_network_security_group.mysecuritygroup.id
 }
--- public-ip.tf
+++ public-ip.tf
@@ -1,6 +1,7 @@
-resource "azurerm_virtual_network" "myipaddress" {
+resource "azurerm_public_ip" "myipaddress" {
   name                = var.ipaddressname
-  address_space       = ["10.0.0.0/16"]
+  allocation_method   = "Static"
   location            = azurerm_resource_group.myTerraformGroup.location
   resource_group_name = azurerm_resource_group.myTerraformGroup.name
+  tags = var.tags
 }
